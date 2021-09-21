f41c9c3f36a4355508136308eba14e994ded6f0d
ianmajkut <ianedwinmajkut@gmail.com>
2021-09-20 22:26:26 -0300

    Se creo la intefaz Productos y se crearon un array de productos y se implemento el codigo para leer todo, eliminar clientes, eliminar productos, eliminar todo y guardar productos

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -6,6 +6,10 @@ interface Clientes{
   edad: number,
 }
 
+interface Productos{
+  nombre: string,
+  precio: number
+}
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
@@ -14,22 +18,33 @@ interface Clientes{
 export class ClientesComponent implements OnInit {
 
   clientes: Array<Clientes> = new Array<Clientes>()
+  productos: Array<Productos> = new Array<Productos>()
 
   constructor() { }
 
   ngOnInit(): void {
-    this.clientes.push(
-    {
-      nombre: "Alejandro",
-      apellido: "Rodriguez",
-      edad: 20
-    },
-    {
-      nombre: "Eusebio",
-      apellido: "Alexander",
-      edad: 22
-    }
-    )
+    // this.clientes.push(
+    // {
+    //   nombre: "Alejandro",
+    //   apellido: "Rodriguez",
+    //   edad: 20
+    // },
+    // {
+    //   nombre: "Eusebio",
+    //   apellido: "Alexander",
+    //   edad: 22
+    // }
+    // )
+    // this.productos.push(
+    // {
+    //   nombre: "Chipa",
+    //   precio: 200
+    // },
+    // {
+    //   nombre: "Carne",
+    //   precio: 300
+    // }
+    // )
   }
 
   guardarCliente()
@@ -38,10 +53,34 @@ export class ClientesComponent implements OnInit {
 
   }
 
-  leerCliente()
+  guardarProductos()
   {
-    localStorage.getItem('clientes')
-    console.log(this.clientes)
+    localStorage.setItem('productos',JSON.stringify(this.productos))
+
   }
+
+  leer()
+  {
+    this.clientes = JSON.parse(localStorage.getItem('clientes')!)
+    this.productos = JSON.parse(localStorage.getItem('productos')!)
+    console.log(this.clientes)
+    console.log(this.productos)
+  }
+
+  eliminarCliente()
+  {
+    localStorage.removeItem('clientes')
+  }
+
+  eliminarProducto()
+  {
+    localStorage.removeItem('productos')
+  }
+
+  eliminarTodo()
+  {
+    localStorage.clear()
+  }
+  
 
 }
