a77c855a49e801b670e5a10064e343940cfb2ec7
ianmajkut <ianedwinmajkut@gmail.com>
2021-09-22 12:06:59 -0300

    Implementacion buscador filtrando por el nombre de los clientes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Clientes } from '../models/clientes';
+import { ClientesService } from '../services/clientes.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  clientes: Array<Clientes> = new Array<Clientes>();
 
-  constructor() { }
+  constructor(public clientesServicio: ClientesService) { }
 
   ngOnInit(): void {
+    this.clientes = this.clientesServicio.clientesLocalStorage
+  }
+
+  buscarClientes(event: any){
+    let nombreBuscar: string = event.target.value; // creacion variable que almacena input 
+    this.clientes = this.clientesServicio.clientesLocalStorage.filter((clienteFiltrar) =>{   
+      //los clientes que tenemos declarados y mostramos en el *ngFor, los filtra y devuelve todos los clientes que tengan contengan lo lo que estamos ingresando
+      return clienteFiltrar.nombre.toLocaleLowerCase().includes(nombreBuscar.toLocaleLowerCase())
+    })
   }
 
 }
